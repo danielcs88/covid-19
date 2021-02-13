d8ff02a09ac1e06960a0e9253c93797123c27c10
Daniel Cárdenas <danielcs88@users.noreply.github.com>
2021-02-13 16:18:59 -0500

    Update

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
-git pull
+# git pull
 
 # Delete all `.DS_Store`
 find . -name ".DS_Store" -delete
@@ -6,9 +6,12 @@ find . -name ".DS_Store" -delete
 # De;ete all `.ipynb_checkpoints` folders
 find . -name .ipynb_checkpoints -type d -exec rm -rf {} \;
 
-jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute --inplace *.ipynb
+# jupyter nbconvert --ExecutePreprocessor.timeout=-1 --to notebook --execute --inplace *.ipynb
+# jupyter nbconvert --to script *.ipynb
 
-jupyter nbconvert --to script *.ipynb
+ipython COVID-19.py
+
+ipython Florida\ R_0.py
 
 black *.py
 
